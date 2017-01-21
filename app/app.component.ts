972a1bee51acb0b58c0e5d840527060e5915bb0e
Asonna <Asonna>
2017-01-20 17:27:59 -0800

    add the function to list foods name on browser; using the data down action up approach

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,58 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Meal Tracker Application</h1>
-    <h3>For Epicodus Angular Code Review</h3>
+    <h1>Meal Tracker Application for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+
+    <food-list [childFoodList]="masterFoodList" (clickSender)="editFood($event)"></food-list>
+    <hr>
+    <div>
+      <div *ngIf="selectedFood">
+        <h3>{{selectedFood.name}}</h3>
+        <p>Food Low in calories? {{selectedFood.low}}</p>
+        <hr>
+        <h3>Edit Food</h3>
+        <label>Enter Food Name:</label>
+        <input [(ngModel)]="selectedFood.name">
+        <label>Enter Food details:</label>
+        <input [(ngModel)]="selectedFood.details">
+        <label>Enter Food calories:</label>
+        <input [(ngModel)]="selectedFood.calorie">
+
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: string = 'Angular2 Code Review';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedFood = null;
 
+  masterFoodList: Food[] = [
+    new Food('Hamburger', 'Did not get a soda or cheese on my burger!', 354),
+    new Food('Fries', 'I only ate half of them.', 365)
+  ];
+
+  // editFood(clickedFood) {
+  //   this.selectedFood = clickedFood;
+  // }
+  //
+  // finishedEditing() {
+  //   this.selectedFood = null;
+  // }
 }
+
+// export class Food {
+//   public low: boolean = false;
+//   constructor(public name: string, public details: string, public calories: number) { }
+// }
---
+++ app/food-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Food } from './food.model';
+
+@Component({
+  selector: 'food-list',
+  template: `
+  <ul>
+  <li (click)="isLow(currentFood)" *ngFor="let currentFood of foods">{{currentFood.name}} <button (click)="editButtonHasBeenClicked(currentFood)">Edit!</button></li>
+  </ul>
+  `
+})
+
+export class FoodListComponent {
+  foods: Food[] = [
+    new Food('Hamburger', "Didn't get a soda or cheese on my burger!", 354),
+    new Food('Fries', 'I only ate half of them.', 365)
+  ];
+  @Input() childFoodList: Food[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(foodToEdit: Food) {
+    this.clickSender.emit(foodToEdit);
+  }
+
+  isLow(clickedFood: Food) {
+    if(clickedFood.low === true) {
+      alert("This food is low in calories!");
+    } else {
+      alert("This food is high in calories. Better get to work!");
+    }
+  }
+
+  calories(currentFood){
+    if (currentFood.calories >= 500){
+      return "bg-danger";
+    } else if (currentFood.calories === 500) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+}
